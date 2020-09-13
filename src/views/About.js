3ff1a4bbaf0c6fae5643903d956097696c360342
liushuanglong <13522002566@163.com>
2020-09-13 15:12:52 +0800

    添加路由----->react-router-dom

---
+++ src/views/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Abot</div>;
+  }
+}
+
+export default About;
---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "./index.scss"
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div className="warp">Home</div>;
+  }
+}
+
+export default Home;
